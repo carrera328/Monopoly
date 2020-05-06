656aabba3f854955e5e6a3aec7c51dc03d0c1f3a
carrera328 <32745195+carrera328@users.noreply.github.com>
2020-05-05 23:57:34 -0400

    US-0004

--- App/controller.js
+++ App/controller.js
@@ -1,11 +1,24 @@
 import * as helper from './helper.js';
 import * as schema from './schema.js';
 
-export function generateEvent(player, rolled) {
-    console.log(`${player.name} has rolled a ${rolled} and landed on ${schema.default.Board[rolled].name}`);
+export function generateEvent(player, incomingRoll) {
+    let actualRoll = incomingRoll.rolled;
+    console.log(`actual roll: ${actualRoll}`);
+    let tileincomingRollOn = schema.default.Board[actualRoll];
+    console.log(tileincomingRollOn.name);
     
-    // is property owned?
-    // stopping point 5/3/2020
-    console.log(`is property owned? : ${schema.default.Board[rolled].name} is owned: ${schema.default.Board[rolled].owner}`);
+    let choice = 'Yes';
+
+    // input validation currently not working 5/5/2020
+
+    if (tileincomingRollOn.owner == null || tileincomingRollOn.owner == undefined) {
+        while (choice !='Yes'); {
+            console.log(`you\'re gonna pay! ${choice}`);
+            let input = prompt(`${player.name} would you like to purchase ${tileincomingRollOn.name} for $${tileincomingRollOn.price}?`);
+            console.log(input);
+        } 
+       
+    }
+    
 }
 
--- App/main.js
+++ App/main.js
@@ -7,8 +7,7 @@ import * as helper from "./helper.js";
 import schema from "./schema.js";
 
 // create 2 players
-
 let playerOne = helper.Player("Casey", 27, []);
 let playerTwo = helper.Player("Sal", 27, []);
 
-controller.generateEvent(playerTwo, 2);
+controller.generateEvent(playerTwo, helper.roll());
